3d3c3caaf4943fce09b76092cb666c6061a74d92
jk <dlwhdrlf0619@naver.com>
2024-04-12 20:30:36 +0900

    add test env

---
+++ test/src/vector_test/vector_test.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include "gtest/gtest.h"
+#include "type_def.h"
+#include "vector_manager.h"
+#include "transformCalc.h"
+
+class TransformTest : public::testing::Test
+{
+    protected:
+        virtual void SetUp()override{}
+        virtual void TearDown()override{}
+};
+
+TEST_F(TransformTest, CreateCustomVector)
+{
+}
