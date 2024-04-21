f101148aef92f353bab9ee98da02a24c3f01c7f9
JK <dlwhdrlf0619@naver.com>
2024-04-21 15:51:06 +0900

    editing button ui test

--- src/GUI.cpp
+++ src/GUI.cpp
@@ -1,3 +1,8 @@
 #ifndef _GUI_H_
 #define _GUI_H_
-#include <Pangolin>
+#include <pangolin/display/display.h>
+#include <pangolin/display/view.h>
+#include <pangolin/handler/handler.h>
+#include <pangolin/gl/gldraw.h>
+
+
--- test/src/GUI_test/gui_test.cpp
+++ test/src/GUI_test/gui_test.cpp
@@ -1,39 +1,65 @@
 #include <iostream>
 #include "gtest/gtest.h"
+#include <pangolin/pangolin.h>
 #include <pangolin/display/display.h>
 #include <pangolin/display/view.h>
 #include <pangolin/handler/handler.h>
 #include <pangolin/gl/gldraw.h>
 
-class guiTest : public::testing::Test
+class guiTest : public ::testing::Test
 {
     protected:
-        virtual void SetUp()override{}
-        virtual void TearDown()override{}
+        virtual void SetUp() override 
+        {
+            pangolin::CreateWindowAndBind("Main", 640, 480);
+            glEnable(GL_DEPTH_TEST);
+
+            s_cam = std::make_unique<pangolin::OpenGlRenderState>(
+                    pangolin::ProjectionMatrix(640, 480, 420, 420, 320, 240, 0.2, 100),
+                    pangolin::ModelViewLookAt(-2, 2, -2, 0, 0, 0, pangolin::AxisY)
+                    );
+
+            handler = std::make_unique<pangolin::Handler3D>(*s_cam);
+            d_cam = &pangolin::CreateDisplay()
+                .SetBounds(0.0, 1.0, 0.0, 1.0, -640.0f / 480.0f)
+                .SetHandler(handler.get());
+        }
+
+        virtual void TearDown() override 
+        {
+            pangolin::QuitAll();
+        }
+
+        std::unique_ptr<pangolin::OpenGlRenderState> s_cam;
+        std::unique_ptr<pangolin::Handler3D> handler;
+        pangolin::View* d_cam;
 };
 
-TEST_F(guiTest, LoadWindow)
+TEST_F(guiTest, LoadWindow) 
 {
-    pangolin::CreateWindowAndBind("Main",640,480);
-    glEnable(GL_DEPTH_TEST);
-
-    pangolin::OpenGlRenderState s_cam(
-            pangolin::ProjectionMatrix(640,480,420,420,320,240,0.2,100),
-            pangolin::ModelViewLookAt(-2,2,-2, 0,0,0, pangolin::AxisY)
-            );
-
-    pangolin::Handler3D handler(s_cam);
-    pangolin::View& d_cam = pangolin::CreateDisplay()
-        .SetBounds(0.0, 1.0, 0.0, 1.0, -640.0f/480.0f)
-        .SetHandler(&handler);
-
-    while( !pangolin::ShouldQuit() )
+    while(!pangolin::ShouldQuit())
     {
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-        d_cam.Activate(s_cam);
-
+        d_cam->Activate(*s_cam);
         pangolin::glDrawColouredCube();
-
         pangolin::FinishFrame();
     }
 }
+
+TEST_F(guiTest, InteractButton) 
+{
+    pangolin::CreatePanel("ui").SetBounds(1.0, 0.0, 0.0, pangolin::Attach::Pix(180));
+    pangolin::Var<bool> button("ui.Button", false, false);
+
+    while(!pangolin::ShouldQuit())
+    {
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+        d_cam->Activate(*s_cam);
+        if (button)
+        {
+            pangolin::glDrawColouredCube();
+            pangolin::FinishFrame();
+        }
+    }
+}
+
